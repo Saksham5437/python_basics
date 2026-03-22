cf5b6d99a47e90678aa2e06b59b0ce7d5b52c47e
Saksham Kulkarni <sakshambk2020@gmail.com>
2026-03-22 16:17:41 +0530

    Add files via upload

---
+++ day4_practice_display_lists.py
@@ -0,0 +1,11 @@
+#Practing with lists and dictionaries with loops
+
+students = ["John", "Jane", "Doe", "Smith"]
+print("Number of students are: ", len(students))
+
+for i in range(len(students)):
+    print("Student", i+1, ":", students[i])
+
+
+
+ 
