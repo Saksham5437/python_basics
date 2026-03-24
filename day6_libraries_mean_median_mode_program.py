dac06f2eab0e30f563cc0198dc9536d89d122536
Saksham Kulkarni <sakshambk2020@gmail.com>
2026-03-24 23:50:51 +0530

    Add files via upload

---
+++ day6_libraries_mean_median_mode_program.py
@@ -0,0 +1,16 @@
+#program to display mean of a given numbers using statistics module
+
+import statistics
+
+mean = statistics.mean([100, 80, 89, 99, 89, 95])
+print(mean)
+
+#program to display median of set of numbers using statistics module
+
+med = statistics.median([45, 56, 72, 15, 26, 19,])
+print(med)
+
+#program to display mode of set of numbers using statistics module
+
+mod = statistics.mode([2, 5, 6, 2, 7, 8, 1, 2, 3, 2])
+print(mod)
